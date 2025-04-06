0dca4f979c348a23f4d4bce59c9769920e32747f
Cameron Olson <cmsolson75@gmail.com>
2025-04-05 20:42:21 -0700

    docker compose

--- training/src/utils.py
+++ training/src/utils.py
@@ -1,16 +1,6 @@
 import torch
 import numpy as np
 import random
-import pandas as pd
-
-
-def load_and_preprocess(path):
-    data = pd.read_csv(path)
-    data.columns = data.columns.str.replace(" ", "")
-    data["status"] = data.loc[:, "loan_status"].map({" Approved": 1, " Rejected": 0})
-    features = ["income_annum", "loan_amount", "loan_term", "cibil_score"]
-    return data[features], data["status"]
-
 
 def set_seed(seed=42):
     torch.manual_seed(seed)
